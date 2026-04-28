9a0f67f9d2c60d1887d3fa3821c2b42c68b1f6e9
rajnagani31 <rajnagani331@gmail.com>
2026-04-28 11:15:09 +0530

    revision signals

--- 08_Ecom/apps/p2p/urls.py
+++ 08_Ecom/apps/p2p/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('user-create/', views.UserCreateAPIView.as_view()),
+    path('product/', views.ProductAPIView.as_view()),
+    path('product/<int:pk>/', views.ProductAPIView.as_view()),
 ]
--- 08_Ecom/apps/p2p/views.py
+++ 08_Ecom/apps/p2p/views.py
@@ -2,7 +2,8 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from .models import User, Email, product, profile
 from rest_framework.response import Response
-
+from .serializers import ProductSerializer
+from pydantic import BaseModel, Field
 
 class UserCreateAPIView(APIView):
     def post(self, request):
@@ -15,9 +16,41 @@ class UserCreateAPIView(APIView):
         user_delete.delete()
         return Response({'message': 'User created successfully'})
     
+class ProductData(BaseModel):
+    name : str
+    price : float
+    description : str
+    data : str | None = None
+
+class ProductDataResponse(BaseModel):
+    name : str
+    price : float
+    description : str
+    data : str | None = None
+        
+class ProductAPIView(APIView):
+    def get(self, request):
+        products = product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response({'message': 'Products fetched successfully', 'data': serializer.data})
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        data_validation = ProductData(**request.data)
+        print('----------------------',data_validation)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        response = ProductDataResponse(
+            name=data_validation.name,
+            price=data_validation.price,
+            description=data_validation.description,
+            data=data_validation.data
+        )
+        # return Response(response.model_dump(), status=201)
+        return Response({'message': 'Product created successfully', 'data': data_validation.model_dump()})
 
 
 
-    
 
     
